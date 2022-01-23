1b9ecd847ae6692e5618255234c6b33314579a5b
Lee Joe Xuen <leejoexuen02@gmail.com>
2022-01-23 18:44:22 +0000

    Update src/electricity/billing/system/DBConnection.java

--- src/electricity/billing/system/DBConnection.java
+++ src/electricity/billing/system/DBConnection.java
@@ -8,9 +8,9 @@ public class DBConnection{
     public DBConnection(){  
         try{  
             Class.forName("com.mysql.cj.jdbc.Driver");  
+             //ebsystem is database name, root is username and no password   
             c =DriverManager.getConnection("jdbc:mysql://localhost:3306/ebsystem","root","");    
             s =c.createStatement();  
-            
            
         }catch(Exception e){ 
             System.out.println(e);
